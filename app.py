d76fc7fe9c9289f6285b13ab9f638773325797e6
Balamurugan <155842573+Balamurugan0113@users.noreply.github.com>
2026-02-26 23:28:45 +0530

    first

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from sqlalchemy import create_engine
-from config import DATABASE_URL
+import os
 
 app = Flask(__name__)
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise Exception("DATABASE_URL not found. Set it in Render Environment Variables.")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}  # required for Supabase
+)
 
 @app.route("/")
 def home():
     return "Backend connected to Supabase"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
